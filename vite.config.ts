21fa458665056a0191e833a3d7bc0c6ecb5e728f
suraj <surajspillai57@gmail.com>
2025-10-20 19:32:50 +0530

    Fix SPA routing on GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : "/Suraj-S-Pillai/", // 👈 Important for GitHub Pages
   server: {
     host: "::",
     port: 8080,
